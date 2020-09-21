745aa9b6701497ae6c3b5dd1b6e609e94d550536
colejagdtiger <coleton.kotch@gmail.com>
2020-09-20 23:21:22 -0600

    Began work on kmeans

--- unsupervisedalgos/kmeans.py
+++ unsupervisedalgos/kmeans.py
@@ -1,12 +1,16 @@
+import numpy as np
+
 
 class KMeans(object):
 
-	def __init__(self, k):
+	def __init__(self, k, high, low):
 		"""
 		implementation of k-means algorithm
 		k = number of clusters
+		high = largest possible cluster initial value
+		low = smallest possible cluster initial value
 		"""
-		self.k = k
+		self.cluster_centroids = np.random.rand(k, dtype='float64') * high - low
 
 	def fitTrainingSet(training_set):
 		"""
